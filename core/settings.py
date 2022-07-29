902a7dd1c53fd8050b71ba734fb2d463073759e2
Igor Mundim <mundim.github@gmail.com>
2022-07-29 18:43:02 -0300

    add config

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("DEBUG") == "1" else False
 
-ALLOWED_HOSTS: ["127.0.0.1:8000"]
+ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
