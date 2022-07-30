0fbd0b8a0fb998eb51789b53038ef1f66811fc28
Igor Mundim <mundim.github@gmail.com>
2022-07-30 10:45:06 -0300

    initial models

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Stock(models.Model):
+    units = models.IntegerField()
+    units.sold = models.IntegerField()
+    last_checked = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.units
