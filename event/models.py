a256c990fc0ba2da35ee53ab63b0dca0a93454e8
Igor Mundim <mundim.github@gmail.com>
2022-08-22 10:34:36 -0300

    add views

--- event/models.py
+++ event/models.py
@@ -1,20 +1,23 @@
+from datetime import date
+from os import truncate
+from typing import Any
+
 from customer.models import Producer, Request
 from django.db import models
+from django.db.models import Count, Q
+from django.utils import timezone
 from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Address(models.Model):
     cep = models.CharField(max_length=8)
-    complement = models.CharField(max_length=150, default="")
-    city = models.CharField(max_length=100, default="")
+    complement = models.CharField(max_length=150)
+    city = models.CharField(max_length=100)
     district = models.CharField(max_length=100)
-    municipality_IBGE = models.IntegerField()
     number = models.IntegerField()
     roud = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     uf = models.CharField(max_length=2)
-    uf_ibge = models.IntegerField()
-    types = models.IntegerField()
 
     def __str__(self):
         return "%s - %s" % (self.city, self.uf)
@@ -29,7 +32,6 @@ class Image(models.Model):
         max_length=200,
         upload_to="covers/%Y/%m/%d/",
         blank=True,
-        default="",
     )
     alt_text = models.CharField(max_length=150)
 
@@ -58,9 +60,8 @@ class Category(models.Model):
         max_length=200,
         upload_to="covers/%Y/%m/%d/",
         blank=True,
-        default="",
     )
-    alt_text = models.CharField(max_length=150, default="")
+    alt_text = models.CharField(max_length=150)
 
     def __str__(self):
         return self.name
@@ -78,13 +79,13 @@ class Event(models.Model):
         Address,
         on_delete=models.PROTECT,
     )
-    categories = models.ManyToManyField(Category, blank=True, default="")
-    image = models.OneToOneField(Image, on_delete=models.PROTECT, null=True)
+    categories = models.ManyToManyField(Category, blank=True)
+    image = models.OneToOneField(Image, on_delete=models.PROTECT)
     name = models.CharField(max_length=100)
     in_room = models.BooleanField(verbose_name="in room", default=True)
     date_end = models.DateTimeField(verbose_name="date end")
     date_start = models.DateTimeField(verbose_name="date start")
-    description = models.CharField(max_length=250, default="")
+    description = models.CharField(max_length=250)
     is_virtual = models.BooleanField(default=False)
     video_url = models.CharField(verbose_name="video url", max_length=200)
     is_published = models.BooleanField(default=True)
@@ -97,15 +98,45 @@ class Event(models.Model):
         verbose_name_plural = "events"
 
 
+class BatchManager(models.Manager):
+    def filter_by_saler(self, *args: Any, **kwargs: Any):
+
+        return (
+            self.filter(
+                Q(event=kwargs.get("event_pk"))
+                & (
+                    Q(sales_qtd__gte=kwargs.get("sales_qtd"))
+                    | Q(batck_stop_date__gte=kwargs.get("batck_stop_date"))
+                )
+            )
+            .order_by("-id")
+            .first()
+        )
+    def is_valid_change(self, *args: Any, **kwargs: Any):
+        event_pk = kwargs.get("event_pk")
+        id = kwargs.get("id")
+        sales_qtd = kwargs.get("sales_qtd")
+        batch_stop_date = kwargs.get("batch_stop_date")
+        batches =  self.filter(event=event_pk)
+        is_valid = True
+        for batch in batches:
+            if(batch.id < id and batch.sales_qtd >= sales_qtd and batch.batck_stop_date >= batch_stop_date):
+                is_valid = False
+            elif(batch.id > id and batch.sales_qtd <= sales_qtd and batch.batck_stop_date <= batch_stop_date):
+                is_valid = False
+        
+        return is_valid
+
 class Batck(models.Model):
+    objects = BatchManager()
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     sequence = models.IntegerField()
-    percentage = models.IntegerField(default="")
-    sales_qtd = models.IntegerField(default=99999)
+    percentage = models.DecimalField(max_digits=3, decimal_places=2)
+    sales_qtd = models.IntegerField()
     batck_start_date = models.DateTimeField(verbose_name="batck start date")
-    batck_stop_date = models.DateTimeField(verbose_name="batck start date")
-    description = models.CharField(max_length=150, default="")
-    is_active = models.BooleanField(True)
+    batck_stop_date = models.DateTimeField(verbose_name="batck stop date")
+    description = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.sequence
@@ -116,8 +147,32 @@ class Batck(models.Model):
         verbose_name_plural = "batches"
 
 
+class TicketLeasingManage(models.Manager):
+    def get_leasing(self, *args: Any, **kwargs: Any):
+        leases = self.filter(event=kwargs.get("event_pk")).annotate(
+            solid_total=Count("units_solid")
+        )
+
+        batck = Batck.objects.filter(
+            Q(event=kwargs.get("event_pk"))
+            & Q(batck_stop_date__gt=timezone.now())
+            & Q(batck_start_date__lte=timezone.now())
+            & Q(sales_qtd__gt=leases[0].solid_total)
+        ).first()
+
+        for leasing in leases:
+            leasing.sale_price = leasing.store_price * (
+                (batck.percentage / 100) + 1
+            )
+            leasing.student_price = (
+                leasing.store_price * ((batck.percentage / 100) + 1)
+            ) / 2
+        return leases
+
+
 class TicketLeasing(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, default="")
+    objects = TicketLeasingManage()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=True)
     descroption = models.CharField(max_length=150, blank=True)
     is_active = models.BooleanField(default=True)
@@ -130,12 +185,11 @@ class TicketLeasing(models.Model):
     student_price = models.DecimalField(
         verbose_name="student price", max_digits=6, decimal_places=2
     )
-    units_solid = models.IntegerField(default="")
-    units = models.IntegerField(default="")
+    units_solid = models.IntegerField()
+    units = models.IntegerField()
 
     def __str__(self):
         return self.name
-
 
 
 class Ticket(models.Model):
@@ -154,25 +208,3 @@ class Ticket(models.Model):
         ordering = ["request_id"]
         verbose_name = "ticket"
         verbose_name_plural = "tickets"
-
-
-class Stock(models.Model):
-    ticket_leasing_id = models.OneToOneField(
-        TicketLeasing,
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-    )
-    units = models.IntegerField()
-    units_sold = models.IntegerField(default=0)
-    last_checked = models.DateTimeField(
-        verbose_name="last checked", auto_now=True
-    )
-
-    def __str__(self):
-        return self.units
-
-    class Meta:
-        ordering = ["ticket_leasing_id"]
-        verbose_name = "stock"
-        verbose_name_plural = "stocks"
--- event_api/serializers.py
+++ event_api/serializers.py
@@ -1,5 +1,5 @@
 from customer.models import Producer
-from event.models import Address, Batck, Category, Event, Image
+from event.models import Address, Batck, Category, Event, Image, TicketLeasing
 from rest_framework import serializers
 
 
@@ -23,12 +23,10 @@ class AddressSerializer(serializers.ModelSerializer):
             "complement",
             "city",
             "district",
-            "municipality_IBGE",
             "number",
             "roud",
             "state",
             "uf",
-            "uf_ibge",
         ]
 
 
@@ -100,11 +98,56 @@ class BatckSerializers(serializers.ModelSerializer):
     class Meta:
         model = Batck
         fields = [
+            "id",
             "event",
             "sequence",
+            "percentage",
             "sales_qtd",
             "batck_start_date",
             "batck_stop_date",
             "description",
+            "is_active",
+        ]
+
+    def validate(self, attrs):
+        sales_qtd = attrs.get("sales_qtd")
+        batch_stop_date = attrs.get("batck_stop_date")
+        event_pk = self.initial_data["event"]
+        if self._context["request"]._stream.method is not "POST":
+
+            batch = Batck.objects.filter_by_saler(
+                event_pk=self.initial_data["event"],
+                sales_qtd=sales_qtd,
+                batck_stop_date=batch_stop_date,
+            )
+            if batch is not None:
+                raise serializers.ValidationError("ERROR")
+            return super().validate(attrs)
+            
+        id = self.instance.id
+        if not Batck.objects.is_valid_change(
+            id=id,
+            sales_qtd=sales_qtd,
+            batch_stop_date=batch_stop_date,
+            event_pk=event_pk,
+        ):
+            raise serializers.ValidationError("ERROR")
+
+        return super().validate(attrs)
+
+
+class TicketLeasingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TicketLeasing
+        fields = [
+            "id",
+            "event",
+            "name",
+            "descroption",
+            "store_price",
+            "sale_price",
+            "student_price",
+            "units_solid",
+            "units",
             "is_active",
         ]
