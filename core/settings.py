f5781b3f2e798c2610efaca98957a86961504f1a
Igor Mundim <mundim.github@gmail.com>
2022-09-06 11:52:22 -0300

    change db

--- core/settings.py
+++ core/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("DATABASE_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("DATABASE_NAME", "db.sqlite3")),
+        "NAME": os.environ.get("DATABASE_NAME", "db.sqlite3"),
         # "ENGINE": "django.db.backends.sqlite3",
         # "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         "USER": os.environ.get("DATABASE_USER"),
